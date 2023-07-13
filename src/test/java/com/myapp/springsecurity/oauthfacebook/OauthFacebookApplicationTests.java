66c493974d0c06b9e96f64ff9b9e0ee6db4d8beb
Nuwan <nuwan199044@gmail.com>
2023-07-13 18:02:14 +0530

    OAuth with facebook

---
+++ src/test/java/com/myapp/springsecurity/oauthfacebook/OauthFacebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.springsecurity.oauthfacebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthFacebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
